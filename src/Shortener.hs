92f2c5b011df3f6e476baffb05de25a7786d5f5a
Anthony Suárez <neolight1010@gmail.com>
2022-01-30 11:02:04 -0500

    Create simple Scotty route.

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -1,5 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Shortener where
+
+import Web.Scotty
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import qualified Text.Blaze.Html5 as H
 
 shortener :: IO ()
 shortener =
-  putStrLn "Hello, Shortener!"
+  scotty 3000 $
+    get "/" $
+      html $ renderHtml $
+        H.docTypeHtml $ do
+          H.head $ do
+            H.title "Shortener"
+          H.body $ do
+            H.h1 "Shortener"
