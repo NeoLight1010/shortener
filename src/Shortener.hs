138dc168fcd113fde2f87d21c49719f7fd0ca4cd
Anthony Suárez <neolight1010@gmail.com>
2022-02-02 16:51:57 -0500

    Add url shortening logic for post "/"

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -7,10 +7,15 @@ import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html5 ((!))
+import Data.IORef (newIORef, modifyIORef')
+import Data.Map (Map, empty, insert)
+import Data.Text (Text)
+import Control.Monad.IO.Class (MonadIO(liftIO))
 
 shortener :: IO ()
 shortener = do
-  scotty 3000 $
+  urlsRef <- newIORef (1 :: Int, empty :: Map Int Text)
+  scotty 3000 $ do
     get "/" $
       html $ renderHtml $
         H.docTypeHtml $ do
@@ -20,3 +25,9 @@ shortener = do
             H.form ! A.method "post" ! A.action "/" $ do
               H.input ! A.type_ "text" ! A.name "url"
               H.input ! A.type_ "submit" ! A.value "Submit"
+    post "/" $ do
+      url <- param "url"
+      liftIO $ modifyIORef' urlsRef $
+        \(i, urls) ->
+          (i + 1, insert i url urls)
+      redirect "/"
