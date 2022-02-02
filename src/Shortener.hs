65411d942285ecb7329cbe28abd1242e6dcad0d1
Anthony Suárez <neolight1010@gmail.com>
2022-02-02 16:36:26 -0500

    Create index URL form.

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -5,14 +5,18 @@ module Shortener where
 import Web.Scotty
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Html5 ((!))
 
 shortener :: IO ()
-shortener =
+shortener = do
   scotty 3000 $
     get "/" $
       html $ renderHtml $
         H.docTypeHtml $ do
-          H.head $ do
+          H.head $
             H.title "Shortener"
           H.body $ do
-            H.h1 "Shortener"
+            H.form ! A.method "post" ! A.action "/" $ do
+              H.input ! A.type_ "text" ! A.name "url"
+              H.input ! A.type_ "submit" ! A.value "Submit"
