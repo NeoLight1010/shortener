470bfe0f5caa2e4a2a7e8bdc863a5060f6ceaf99
Anthony Suárez <neolight1010@gmail.com>
2022-02-02 17:36:21 -0500

    Render saved urls on index.

--- src/Shortener.hs
+++ src/Shortener.hs
@@ -7,16 +7,18 @@ import Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html5 ((!))
-import Data.IORef (newIORef, modifyIORef')
-import Data.Map (Map, empty, insert)
+import Data.IORef (newIORef, modifyIORef', readIORef)
+import Data.Map (Map, empty, insert, toList)
 import Data.Text (Text)
 import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.Foldable (for_)
 
 shortener :: IO ()
 shortener = do
   urlsRef <- newIORef (1 :: Int, empty :: Map Int Text)
   scotty 3000 $ do
-    get "/" $
+    get "/" $ do
+      (_, urls) <- liftIO $ readIORef urlsRef
       html $ renderHtml $
         H.docTypeHtml $ do
           H.head $
@@ -25,6 +27,11 @@ shortener = do
             H.form ! A.method "post" ! A.action "/" $ do
               H.input ! A.type_ "text" ! A.name "url"
               H.input ! A.type_ "submit" ! A.value "Submit"
+          H.table $ do
+            for_ (toList urls) $ \(i, url) ->
+              H.tr $ do
+                H.td (H.toHtml i)
+                H.td (H.toHtml url)
     post "/" $ do
       url <- param "url"
       liftIO $ modifyIORef' urlsRef $
